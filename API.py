66cc19e2b64cf392533005558a101a6987640b4a
DaichiSawamura <dimakr-303@yandex.ru>
2023-03-01 16:46:16 +0300

    add +, <, >

--- API.py
+++ API.py
@@ -49,11 +49,30 @@ class Channel:
     def get_id(self):
         return self.__id
 
+    def __str__(self):
+        return f"Youtube-канал: {self.title}"
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
 
 chn1 = Channel("UC2Ru64PHqW4FxoP0xhQRvJg")
+chn2 = Channel("UC8M5YVWQan_3Elm-URehz9w")
 chn1.get_info()
-print(chn1.title)
-print(chn1.video_count)
-print(chn1.url)
-print(chn1.get_service())
-chn1.to_json('chn1.json')
+chn2.get_info()
+# print(chn1.title)
+# print(chn1.video_count)
+# print(chn1.url)
+# print(chn1.get_service())
+# chn1.to_json('chn1.json')
+print(chn1)
+print(chn2)
+print(chn1 > chn2)
+print(chn1 < chn2)
+print(chn1 + chn2)
